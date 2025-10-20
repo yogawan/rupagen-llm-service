a6b43c727309984f36add1e496240ec18af2a14b
Yogaone <yogawanadityapratama@gmail.com>
2025-10-20 19:28:15 +0700

    Setup project #2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Head>
+        <title>Mintrix</title>
+        <link rel="icon" type="image/png" href="/favicon/icon.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+export default App;
