0e5938134c31020bd989606351e44e0e964f46b2
Yogaone <yogawanadityapratama@gmail.com>
2025-11-07 01:37:56 +0700

    Add new achievement image (6.png) to the public/pencapaian directory

--- pages/parent/[id]/index.tsx
+++ pages/parent/[id]/index.tsx
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Splash from "@/components/Splash";
+import Image from "next/image";
 
 const ParentPage = () => {
   const { id } = useRouter().query;
@@ -34,10 +35,6 @@ const ParentPage = () => {
 
   return (
     <div style={{ padding: "20px" }}>
-      <h1>Parent Profile Detail</h1>
-
-      {/* USER */}
-      <h2>User</h2>
       <img
         src={user.foto}
         alt={user.nama}
@@ -48,51 +45,102 @@ const ParentPage = () => {
         <strong>Nama:</strong> {user.nama}
       </p>
       <p>
-        <strong>Email:</strong> {user.email}
-      </p>
-      <p>
-        <strong>Personalization:</strong> {user.personalization ? "Yes" : "No"}
+        <strong>Id:</strong> {user._id}
       </p>
 
-      {/* STATS */}
-      <h2>Stats</h2>
-      <p>
-        <strong>Streak Active:</strong>{" "}
-        {stats.streakActive ? "Active" : "Not Active"}
-      </p>
-      <p>
-        <strong>Streak Count:</strong> {stats.streakCount}
-      </p>
-      <p>
-        <strong>Point:</strong> {stats.point}
-      </p>
-      <p>
-        <strong>XP:</strong> {stats.xp}
-      </p>
-      <p>
-        <strong>Liga:</strong> {stats.liga}
-      </p>
+      <div className="flex gap-5">
+        <p>
+          <strong>Liga:</strong> {stats.liga}
+        </p>
+        <p>
+          <strong>XP:</strong> {stats.xp}
+        </p>
+        <p>
+          <strong>Runtutan Hari:</strong> {stats.streakCount}
+        </p>
+      </div>
 
-      {/* PERSONALITY */}
-      <h2>Personality</h2>
-      <p>
-        <strong>Kreatifitas:</strong> {personality.kreatifitas}
-      </p>
-      <p>
-        <strong>Keberanian:</strong> {personality.keberanian}
-      </p>
-      <p>
-        <strong>Empati:</strong> {personality.empati}
-      </p>
-      <p>
-        <strong>Kerja Sama:</strong> {personality.kerjaSama}
-      </p>
-      <p>
-        <strong>Tanggung Jawab:</strong> {personality.tanggungJawab}
-      </p>
-      <p>
-        <strong>Hobi & Minat:</strong> {personality.hobiDanMinat}
-      </p>
+      <h2 className="mt-5 text-xl font-bold">Kepripadian Kamu</h2>
+      <div>
+        <p>
+          Data di bawah ini merupakan rekam jejak penggunaan aplikasi dari awal
+          hingga saat ini dan dapat berubah kapanpun. Penilaian yang ditampilkan
+          didasarkan pada keseluruhan aktivitas anak.
+        </p>
+      </div>
+
+      <h2 className="mt-5 text-xl font-bold">Pencapaian</h2>
+      <div className="flex">
+        <Image
+          src="/pencapaian/1.png"
+          alt="Pencapaian 1"
+          width={120}
+          height={120}
+        />
+        <Image
+          src="/pencapaian/2.png"
+          alt="Pencapaian 2"
+          width={120}
+          height={120}
+        />
+        <Image
+          src="/pencapaian/3.png"
+          alt="Pencapaian 3"
+          width={120}
+          height={120}
+        />
+        <Image
+          src="/pencapaian/4.png"
+          alt="Pencapaian 4"
+          width={120}
+          height={120}
+        />
+        <Image
+          src="/pencapaian/5.png"
+          alt="Pencapaian 5"
+          width={120}
+          height={120}
+        />
+        <Image
+          src="/pencapaian/6.png"
+          alt="Pencapaian 6"
+          width={120}
+          height={120}
+        />
+      </div>
+
+      <h2 className="mt-5 text-xl font-bold">Perkembangan</h2>
+      <div>
+        <p>
+          <strong>Kreatifitas:</strong> {personality.kreatifitas}
+        </p>
+        <p>
+          <strong>Keberanian:</strong> {personality.keberanian}
+        </p>
+        <p>
+          <strong>Empati:</strong> {personality.empati}
+        </p>
+        <p>
+          <strong>Kerja Sama:</strong> {personality.kerjaSama}
+        </p>
+        <p>
+          <strong>Tanggung Jawab:</strong> {personality.tanggungJawab}
+        </p>
+
+        <h2 className="mt-5 text-xl font-bold">Hobi & Minat Kamu</h2>
+        <div>
+          <p>{personality.hobiDanMinat}</p>
+        </div>
+
+        <h2 className="mt-5 text-xl font-bold">Sifat dan kepribadian</h2>
+        <div>
+          <p>
+            Kamu lebih nyaman dan bersemangat saat punya waktu untuk sendiri dan
+            merenung. memiliki banyak ide unik, mandiri, dan sangat teliti
+            dengan hasil kerjanya.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
